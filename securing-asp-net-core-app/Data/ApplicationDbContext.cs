0f630f6cc12e442e6748e3732c72441dac5be5b6
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2023-02-10 11:04:00 +0530

    Add project files.

---
+++ securing-asp-net-core-app/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace securing_asp_net_core_app.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ securing-asp-net-core-app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace securing_asp_net_core_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
