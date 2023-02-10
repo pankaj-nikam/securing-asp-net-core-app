d7924ddf30515b1c97ac3badd3c0d18c3d793980
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2023-02-10 17:13:15 +0530

    Revert "A101-Items"
    This reverts commit 937f17d380418e66fb1aaa10603d95d7896e6aa2.

--- securing-asp-net-core-app/Models/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace securing_asp_net_core_app.Models
-{
-    public static class Constants
-    {
-        public const string ADMIN_ROLE_NAME = "Administrator";
-        public const string EMPLOYEE_ROLE_NAME = "Employee";
-    }
-}
